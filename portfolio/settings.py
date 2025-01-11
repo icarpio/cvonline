5d64e7d12313ad047932bf1495a0ce3b142839d9
Ivan <icarpiodeveloper@gmail.com>
2025-01-11 21:34:38 +0100

    Add Icons

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition
