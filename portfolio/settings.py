fd74928254c2daa5e4b8a6ba8878a26cbe289043
Ivan <icarpiodeveloper@gmail.com>
2025-01-11 17:27:10 +0100

    Fix Env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://cvonline-p06s.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost,127.0.0.1').split(',')
 
 
 # Application definition
